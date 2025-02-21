ddf206bf0226bf89261180f727d93499fd8acdf8
jluiz-programacao <202503027871@alunos.estacio.br>
2025-02-21 19:48:33 +0000

    Comecei com o meu printf

--- cartas.c
+++ cartas.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+    int  main(){
+        int carta; 
+        char A,B,C,D,E,F,G,H; //estados//
+        char A01[10], B04[10]; //codigo da carta//
+        char Fortaleza[10], Juazeiro[10];
+        int Populacao;
+        float Area; //em Km²//
+        float PIB;
+        int Numero_de_pontos_turisticos; 
+
+            printf("Digite o número da sua carta: \n");
+            scanf("%d", &carta);
+
+            printf("Digite a letra corespondente ao Estado: \n");
+            scanf("%s", &A, &B);
+
+            printf("Digite o código da sua carta: \n");
+            scanf("%s", &A01, &B04);
+
+
+    return 0;
+
+}    
